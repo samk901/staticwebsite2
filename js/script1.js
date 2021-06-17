d590ff9f2a0a8c268f7cc8ecb40f920b3c4408c4
samk90 <kim.donghy@northeastern.edu>
2021-06-17 11:18:44 -0400

    added CSS to button

--- js/script1.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener("DOMContentLoaded", function(event){
-  document.getElementById("div1").textContent = "Document DOMContentLoaded event happened!"
-});
-
-// Occurs after everything has loaded
-window.onload = function() {
-  document.getElementById("div2").textContent = "Window.onload event happened"
-}
