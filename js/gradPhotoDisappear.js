0222e2089cec1bfe68b902c6df42687f2b2cfd34
samk90 <kim.donghy@northeastern.edu>
2021-06-17 10:14:30 -0400

    fixed photo toggling

--- js/gradPhotoDisappear.js
+++ js/gradPhotoDisappear.js
@@ -1,9 +1,14 @@
 $("#gradPhoto").on("click", function() {
   $( "#gradPhoto").animate({
-    opacity: 0.25,
     height: "toggle"
-  }, 1000, function() {
-
-  },
+  }, 1000,
   console.log('clicked grad photo'));
+  $( "#h1").append( "<button id=\"photoButton\">Reappear Photo</button>");
+});
+
+$( "#photoButton").on("click", function() {
+  $( "#gradPhoto").animate({
+    height: "toggle"
+  }, 1000,
+  console.log('clicked button'));
 });
